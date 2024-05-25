d15aa088b41c252263a444227c30a6f67fa1b5dd
Evin Jaff <evin@jaffboys.com>
2024-05-25 14:16:57 -0700

    Update filepaths to work on Linux

--- pkhex/pkhex-egglocke-tests/pkhex-egglocke-tests/testConstants.cs
+++ pkhex/pkhex-egglocke-tests/pkhex-egglocke-tests/testConstants.cs
@@ -10,12 +10,12 @@ namespace pkhexEgglockeTests
     public static class testConstants
     {
         // Gen 4 - Soul Silver Blank Save File
-        public static string BLANK_SOULSILVER_SAVE => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"testSources\emptySoulSilver.sav");
+        public static string BLANK_SOULSILVER_SAVE => Path.Combine(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"testSources"), "emptySoulSilver.sav");
         public static string BLANK_SOULSILVER_OT_STRING => "Egg";
 
 
         // Gen 6 - Omega Ruby Save File
-        public static string BLANK_OMEGARUBY_SAVE => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"testSources\emptyOmegaRuby.sav");
+        public static string BLANK_OMEGARUBY_SAVE => Path.Combine(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"testSources"), "emptyOmegaRuby.sav");
 
 
     }
