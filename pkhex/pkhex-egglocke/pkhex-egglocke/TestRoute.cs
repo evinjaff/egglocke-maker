bbab9ebcca1e337b4ab6959dc785f1821fa5bfde
Evin Jaff <evin@wustl.edu>
2024-06-08 02:06:42 -0500

    Add file download endpoint (just pokemon theme for now)

--- pkhex/pkhex-egglocke/pkhex-egglocke/TestRoute.cs
+++ pkhex/pkhex-egglocke/pkhex-egglocke/TestRoute.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Grapevine;
+using PKHeX.Core;
 
 namespace pkhex_egglocke
 {
@@ -15,5 +16,25 @@ namespace pkhex_egglocke
         {
             await context.Response.SendResponseAsync("Successfully hit the test route!");
         }
+
+        [RestRoute("Get", "/api/startfile")]
+        public async Task StartFile(IHttpContext context)
+        {
+            await context.Response.SendResponseAsync("Successfully hit the startfile route!");
+        }
+
+        [RestRoute("Get", "/api/file1")]
+        public async Task DownloadFile(IHttpContext context)
+        {
+            string filePath = "C:\\Users\\Evin Jaff\\Downloads\\Pokemon.mp3";
+
+            context.Response.ContentType = "audio/mpeg";
+            context.Response.AddHeader("Content-Disposition", "attachment; filename=blank.mp3");
+            context.Response.ContentLength64 = new FileInfo(filePath).Length;
+
+            await context.Response.SendResponseAsync(File.ReadAllBytes(filePath));
+
+        }
+
     }
 }
